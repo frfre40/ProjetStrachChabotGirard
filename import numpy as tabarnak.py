8f1615a06c76efab8864ca627b196bc2c69b511d
frfre40 <149911168+frfre40@users.noreply.github.com>
2026-03-18 21:12:57 -0400

    Création des classes Electron, Hamiltonien et FockOperator

--- import numpy as tabarnak.py
+++ import numpy as tabarnak.py
@@ -1,1 +1,19 @@
-import numpy as tabarnak
+import numpy as np
+
+
+class Electron:
+
+    def __init__(self, orbitale, spin):
+        self.orbitale = orbitale
+        self.spin = spin
+
+
+class Hamiltonien:
+
+    def __init__(self, nbNoyaux, nbElectrons):
+        self.nbNoyaux = nbNoyaux
+        self.nbElectrons = nbElectrons
+
+class FockOperator:
+
+    def __init__(self, Hamiltonien, Coulomb, Exchange)
