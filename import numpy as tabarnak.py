7091ecd836919b1c82984fe96a3ebd9c49f54639
frfre40 <149911168+frfre40@users.noreply.github.com>
2026-03-27 14:43:11 -0400

    gros changement (j'ai tout deleté)

--- import numpy as tabarnak.py
+++ import numpy as tabarnak.py
@@ -1,19 +1,19 @@
-import numpy as np
+import pyscf as scf
+
+###Initialisation de la molécule, de sa géométrie et de la base utilisée pour chaque atome
+def constructionMolecule(element, basis, r):
+    molecule = scf.gto.Mole()
+    molecule.atom = f'''{element} 0 0 0; {element} {r} 0 0'''
+    molecule.basis = basis
+
+    return molecule
+
+xavier = constructionMolecule('H', 'sto-3g', 1.0)
 
 
-class Electron:
-
-    def __init__(self, orbitale, spin):
-        self.orbitale = orbitale
-        self.spin = spin
 
 
-class Hamiltonien:
 
-    def __init__(self, nbNoyaux, nbElectrons):
-        self.nbNoyaux = nbNoyaux
-        self.nbElectrons = nbElectrons
 
-class FockOperator:
 
-    def __init__(self, Hamiltonien, Coulomb, Exchange)
+    
