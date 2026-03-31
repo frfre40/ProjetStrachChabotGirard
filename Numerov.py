09053f9106457a760aecf2a9d8a2aacd41c4091b
frfre40 <149911168+frfre40@users.noreply.github.com>
2026-03-31 11:46:27 -0400

    petit gossage sur numerov

--- Numerov.py
+++ Numerov.py
@@ -2,7 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def helloworld(string):
-    print(string)
+def Numerov(psi0, xi, xf, dx, potentiel):
+    psi = np.array([psi0], float)
 
-helloworld("print")
+    for i in range(1, len(np.arange(xi, xf, dx))) - 2:
+        A = 2 * (1 - 5 * dx**2/12* potentiel[i]) * psi[i]
+        B = (1 + dx**2/12*potentiel[i - 1]) * psi[i - 1]
+        C = (1 + dx**2/12*potentiel[i + 1])
+    
+
+
